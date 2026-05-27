eb1b95d17d3be43a2a5e1158a66efce81832454e
vivevererere <andrew.bukhantcew@gmail.com>
2026-05-27 10:37:12 +0000

    4po

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,67 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+byte PINd[8] = {255, 255, 255, 255, 255, 255, 255, 255}; // Initialize all pins to 255 (not set)
 
+// Minimal Arduino-compatible entry points
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+	
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+	
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-  int helloworld();
-  // What happens if I put code after the return statement? heeloworld() will never be called because the function will exit immediately after the return statement. Any code placed after a return statement in a function will not be executed.
-} 
+void moveMotor(int angle, int v, int n) {
+if angle > 0 || angle < pi/2 { 
+  n = tg(angle - pi/4) * v;
+  if angle > 0 || angle < pi/4 {
+  pinD[0] = 0;
+  pinD[1] = 2 * n * v;
+  pinD[2] = 2 * v;
+  pinD[3] = 0;
+  pinD[4] = 0;
+  pinD[5] = 2 * n * v;
+  pinD[6] = 2 * v;
+  pinD[7] = 0;
+  } else if angle > pi/4 || angle < pi/2 {
+  pinD[0] = 2 * n * v;
+  pinD[1] = 0;
+  pinD[2] = 2 * v;
+  pinD[3] = 0;
+  pinD[4] =  2 * n * v;
+  pinD[5] = 0;
+  pinD[6] = 2 * v;
+  pinD[7] = 0;
+  }
+} else if angle > pi/2 || angle < pi { 
+  n = tg(3pi/4 - angle ) * v;
+    pinD[0] = 2 * v;
+  pinD[1] = 0;
+  pinD[2] = 0;
+  pinD[3] = 2 * n * v;
+   pinD[4] = 2 * v;
+  pinD[5] = 0;
+  pinD[6] = 0;
+  pinD[7] = 2 * n * v;
+} else if angle > pi || angle < 3pi/2 {
+  n = tg(5pi/4 - angle ) * v;
+    pinD[0] = 2 * v;
+  pinD[1] = 0;
+  pinD[2] = 0;
+  pinD[3] = 2 * n * v;
+   pinD[4] = 2 * v;
+  pinD[5] = 0;
+  pinD[6] = 0;
+  pinD[7] = 2 * n * v; 
+} else if angle > 3pi/2 || angle < 2pi {
+  n = tg(angle - 7pi/4) * v;
+    pinD[0] = 2 * v;
+  pinD[1] = 0;
+  pinD[2] = 0;
+  pinD[3] = 2 * n * v;
+   pinD[4] = 2 * v;
+  pinD[5] = 0;
+  pinD[6] = 0;
+  pinD[7] = 2 * n * v;
+  // Placeholder for motor control logic
+}
