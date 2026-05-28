31dc5db3e1c2672526674036ce633a42f216fa96
Rodikolion <rodion2007a@mail.ru>
2026-05-28 20:56:05 +0300

    моторчики

--- src/main.cpp
+++ src/main.cpp
@@ -1,67 +1,78 @@
 #include <Arduino.h>
 
-byte PINd[8] = {255, 255, 255, 255, 255, 255, 255, 255}; // Initialize all pins to 255 (not set)
+byte PINd[8] = {255, 255, 255, 255, 255, 255, 255, 255}; 
 
 // Minimal Arduino-compatible entry points
-void setup() {
-	
+void setup()
+{
 }
 
-void loop() {
-	
+void loop()
+{
 }
 
-void moveMotor(int angle, int v, int n) {
-if angle > 0 || angle < pi/2 { 
-  n = tg(angle - pi/4) * v;
-  if angle > 0 || angle < pi/4 {
-  pinD[0] = 0;
-  pinD[1] = 2 * n * v;
-  pinD[2] = 2 * v;
-  pinD[3] = 0;
-  pinD[4] = 0;
-  pinD[5] = 2 * n * v;
-  pinD[6] = 2 * v;
-  pinD[7] = 0;
-  } else if angle > pi/4 || angle < pi/2 {
-  pinD[0] = 2 * n * v;
-  pinD[1] = 0;
-  pinD[2] = 2 * v;
-  pinD[3] = 0;
-  pinD[4] =  2 * n * v;
-  pinD[5] = 0;
-  pinD[6] = 2 * v;
-  pinD[7] = 0;
+void moveMotor(int angle, int v, int n)
+{
+   
+  if (angle > 0 || angle < PI / 2)
+  {
+    n = tan(angle - PI / 4) * v;
+    if (angle > 0 || angle < PI / 4)
+    {
+      pinD[0] = 0;
+      pinD[1] = 2 * n * v;
+      pinD[2] = 2 * v;
+      pinD[3] = 0;
+      pinD[4] = 0;
+      pinD[5] = 2 * n * v;
+      pinD[6] = 2 * v;
+      pinD[7] = 0;
+    }
+    else if (angle > PI / 4 || angle < PI / 2)
+    {
+      pinD[0] = 2 * n * v;
+      pinD[1] = 0;
+      pinD[2] = 2 * v;
+      pinD[3] = 0;
+      pinD[4] = 2 * n * v;
+      pinD[5] = 0;
+      pinD[6] = 2 * v;
+      pinD[7] = 0;
+    }
   }
-} else if angle > pi/2 || angle < pi { 
-  n = tg(3pi/4 - angle ) * v;
+  else if (angle > PI / 2 || angle < PI)
+  {
+    n = tan(3 * PI / 4 - angle) * v;
     pinD[0] = 2 * v;
-  pinD[1] = 0;
-  pinD[2] = 0;
-  pinD[3] = 2 * n * v;
-   pinD[4] = 2 * v;
-  pinD[5] = 0;
-  pinD[6] = 0;
-  pinD[7] = 2 * n * v;
-} else if angle > pi || angle < 3pi/2 {
-  n = tg(5pi/4 - angle ) * v;
+    pinD[1] = 0;
+    pinD[2] = 0;
+    pinD[3] = 2 * n * v;
+    pinD[4] = 2 * v;
+    pinD[5] = 0;
+    pinD[6] = 0;
+    pinD[7] = 2 * n * v;
+  }
+  else if (angle > PI || angle < 3 * PI / 2)
+  {
+    n = tan(5 * PI / 4 - angle) * v;
     pinD[0] = 2 * v;
-  pinD[1] = 0;
-  pinD[2] = 0;
-  pinD[3] = 2 * n * v;
-   pinD[4] = 2 * v;
-  pinD[5] = 0;
-  pinD[6] = 0;
-  pinD[7] = 2 * n * v; 
-} else if angle > 3pi/2 || angle < 2pi {
-  n = tg(angle - 7pi/4) * v;
+    pinD[1] = 0;
+    pinD[2] = 0;
+    pinD[3] = 2 * n * v;
+    pinD[4] = 2 * v;
+    pinD[5] = 0;
+    pinD[6] = 0;
+    pinD[7] = 2 * n * v;
+  }
+  else if (angle > 3 * PI / 2 || angle < 2 * PI)
+  {
+    n = tan(angle - 7 * PI / 4) * v;
     pinD[0] = 2 * v;
-  pinD[1] = 0;
-  pinD[2] = 0;
-  pinD[3] = 2 * n * v;
-   pinD[4] = 2 * v;
-  pinD[5] = 0;
-  pinD[6] = 0;
-  pinD[7] = 2 * n * v;
-  // Placeholder for motor control logic
-}
+    pinD[1] = 0;
+    pinD[2] = 0;
+    pinD[3] = 2 * n * v;
+    pinD[4] = 2 * v;
+    pinD[5] = 0;
+    pinD[6] = 0;
+    pinD[7] = 2 * n * v;
+  }
